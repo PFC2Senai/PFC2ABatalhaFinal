608b30e2779756735b73324c5551dba0f86d115c
WilhamJr <WilhamJr@WilhamJr-PC>
2016-01-28 00:20:45 -0200

    Merge origin/master
    Conflicts:
    	src/funcoes/Conexao.java
    	src/funcoes/UsuarioDAO.java

--- src/telas/ExibeDetVenda.java
+++ src/telas/ExibeDetVenda.java
@@ -9,6 +9,7 @@ import atributos.Vendas;
 import static funcoes.Conexao.getConnection;
 import funcoes.ModeloTabela;
 import funcoes.VendasDAO;
+import java.awt.Color;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,57 +18,73 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import static telas.ExibeVenda.GetIndice;
 
 /**
  *
  * @author WilhamJr
  */
-public class ExibeVenda extends javax.swing.JFrame {
-    
-    Statement stmt ;
-    Vendas vendas = new Vendas();
-    private static int indice;
-    /**
+public class ExibeDetVenda extends javax.swing.JFrame {
 
+    Statement stmt ;
+    int idContato;
+    int idDetVenda;
+    Vendas vendas = new Vendas();
+    private final int codVenda;
+    private static int indice;
+    private ExibeDetVenda telaDetalVenda;
+    
     /**
-     * Creates new form ExibeVenda
+     * Creates new form ExibeDetVenda
      */
-    public ExibeVenda() {
+    
+    public ExibeDetVenda() {
         initComponents();
-        TabelaVendas("select * from tabvendas;"); 
+        //this.idContato = VendasDAO.idDetVenda(GetIndice());
+        this.codVenda = GetIndice();
+        telaDetalVenda = this;
+        TabelaVendas();
+//        CarregaVenda();
+        /*initComponents();
+        CarregaCliente();
+        populaComboBox();
+        TabelaLembrete2(GetIndice());
+        TabelaContatos();
+        TabelaEquipamentosCli();
+        TabelaRotina("select  * from tabrotinacontato where cliente_idcliente = " + codVenda + ";");
+        Color minhaCor = new Color(217, 228, 241);
+        this.getContentPane().setBackground(minhaCor);   */
+        //TabelaVendas("select * from tabvendas inner join tabdetvendas on tabvendas.idtabVendas = "
+          //      + "tabdetvendas.tabVendas_idtabVendas;");
     }
     
-    public static int GetIndice() {         
-        return indice;
-    }
     
-    public void TabelaVendas(String Sql){
+    public void TabelaVendas(){
         
         try {
+            String Sql = "select * from tabvendas inner join tabdetvendas on "
+                    + "tabvendas.idtabVendas = tabdetvendas.tabVendas_idtabVendas = " + codVenda + ";";
+            
             stmt = getConnection().createStatement();
             ArrayList dados = new ArrayList();               
             String [] Colunas = {"Código da Venda","Código do cliente", "Código do Usuário", "Data da Venda",
-                "Hora da Venda", "Código do Serviço", "Total da venda"};
+                "Hora da Venda", "Código do Serviço", "Total da venda", "Código do Detalhe da Venda", 
+                "Quantidade", "Código do Detalhe do Produto"};
                
             ResultSet rs;
-            rs = stmt.executeQuery(Sql);            
-            rs.first();
+            rs = stmt.executeQuery(Sql);
             
-            do{
+            while(rs.next()){
                dados.add(new Object[]{rs.getObject("idtabVendas"),rs.getObject("cliente_idcliente"),rs.getObject("tabusuario_id_usuario")
-                       ,rs.getObject("dataVenda"),rs.getObject("hora"),rs.getObject("tabordemserv_idtabOrdemServ"), rs.getObject("totalVenda")});            
-            }while(rs.next());
+                       ,rs.getObject("dataVenda"),rs.getObject("hora"),rs.getObject("tabordemserv_idtabOrdemServ"), rs.getObject("totalVenda")
+               ,rs.getObject("idtabDetVendas"), rs.getObject("quantidade"), rs.getObject("tabdetproduto_idDetProduto")});            
+            }
                         
-            for (int i = 0; i < 7; i++){
+            for (int i = 0; i < 10; i++){
                 ModeloTabela modelo = new ModeloTabela(dados, Colunas);
                 jTableListarVendas.setModel(modelo);
-                jTableListarVendas.getColumnModel().getColumn(0).setPreferredWidth(200);
-                jTableListarVendas.getColumnModel().getColumn(1).setPreferredWidth(200);
-                jTableListarVendas.getColumnModel().getColumn(2).setPreferredWidth(200);
-                jTableListarVendas.getColumnModel().getColumn(3).setPreferredWidth(200);
-                jTableListarVendas.getColumnModel().getColumn(4).setPreferredWidth(200);
-                jTableListarVendas.getColumnModel().getColumn(5).setPreferredWidth(200);
-                jTableListarVendas.getColumnModel().getColumn(6).setPreferredWidth(200);
+                jTableListarVendas.getColumnModel().getColumn(i).setPreferredWidth(200);
                 jTableListarVendas.getColumnModel().getColumn(i).setResizable(false);
                 jTableListarVendas.getTableHeader().setReorderingAllowed(false);
                 jTableListarVendas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -76,23 +93,6 @@ public class ExibeVenda extends javax.swing.JFrame {
         } catch (SQLException ex) {
             Logger.getLogger(ExibeVenda.class.getName()).log(Level.SEVERE, null, ex);
         }             
-    }
-    
-    
-    
-    public boolean CodigoVenda() {
-
-        boolean valida = false;
-
-        if (jTableListarVendas.getSelectedRow() != -1) {
-            valida = true;
-            //this.dispose();
-            int linha = jTableListarVendas.getSelectedRow();
-            indice = (Integer.parseInt(jTableListarVendas.getValueAt(linha, 0).toString()));
-        } else {
-            JOptionPane.showMessageDialog(null, "Primeiro selecione um registro.");
-        }
-        return valida;
     }
 
     /**
@@ -110,12 +110,11 @@ public class ExibeVenda extends javax.swing.JFrame {
         jButton7 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTableListarVendas = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("Cadastro de Vendas");
+        jLabel1.setText("Detalhe de Vendas");
 
         jButton3.setText("Novo");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
@@ -152,14 +151,6 @@ public class ExibeVenda extends javax.swing.JFrame {
         jTableListarVendas.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
         jScrollPane2.setViewportView(jTableListarVendas);
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jButton1.setText("Detalhar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -168,46 +159,38 @@ public class ExibeVenda extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane2)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 831, Short.MAX_VALUE)
                         .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(368, 368, 368))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addGap(314, 314, 314))))))
+                        .addComponent(jButton3)
+                        .addGap(51, 51, 51)
+                        .addComponent(jBtnEditarUsuario)
+                        .addGap(46, 46, 46)
+                        .addComponent(jButton7)
+                        .addGap(274, 274, 274))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(327, Short.MAX_VALUE)
-                .addComponent(jButton3)
-                .addGap(51, 51, 51)
-                .addComponent(jBtnEditarUsuario)
-                .addGap(46, 46, 46)
-                .addComponent(jButton7)
-                .addGap(288, 288, 288))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(312, 312, 312))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
+                .addGap(21, 21, 21)
                 .addComponent(jLabel1)
-                .addGap(61, 61, 61)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addGap(44, 44, 44)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jButton7)
                         .addComponent(jButton3))
                     .addComponent(jBtnEditarUsuario, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addGap(19, 19, 19))
+                .addGap(36, 36, 36))
         );
 
         pack();
-        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
@@ -227,20 +210,11 @@ public class ExibeVenda extends javax.swing.JFrame {
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
         this.dispose();
+        new ExibeVenda().setVisible(true);
     }//GEN-LAST:event_jButton7ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        if (CodigoVenda()) {
-            new ExibeDetVenda().setVisible(true);
-        }
-        
-        //this.dispose();
-        //new ExibeDetVenda().setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBtnEditarUsuario;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton7;
     private javax.swing.JLabel jLabel1;
