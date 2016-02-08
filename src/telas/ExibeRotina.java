1721eb7cc93e165ad8d53f0ff6bbfab6e0740d55
VINICIOS <VINICIOS@VINICIOS-PC>
2016-02-08 09:00:49 -0300

    Merge origin/master
    Conflicts:
    	src/telas/AlterarFabricante.form
    	src/telas/AlterarFabricante.java

--- src/telas/ExibeRotina.java
+++ src/telas/ExibeRotina.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package telas;
 
 import atributos.RotinaContatos;
@@ -16,13 +21,11 @@ import javax.swing.ListSelectionModel;
  * @author WilhamJr
  */
 public class ExibeRotina extends javax.swing.JFrame {
-
-    Statement stmt;
-    RotinaContatos rotinaContatos = new RotinaContatos();
-    private static int indice;
-    private static int idContato;
-    private int idCliente;
-    private DetalharCliente telaDetalharCliente;
+    
+    Statement stmt ;
+RotinaContatos rotinaContatos = new RotinaContatos();
+private static int indice;
+private static int idContato;
 
     /**
      * Creates new form ExibeRotina
@@ -31,62 +34,47 @@ public class ExibeRotina extends javax.swing.JFrame {
         initComponents();
         TabelaRotina("select  * from tabrotinacontato;");
     }
-
-    public ExibeRotina(int codCliente, DetalharCliente detCli) {
-        this.idCliente = codCliente;
-        telaDetalharCliente = detCli;
-        initComponents();
-        TabelaRotina("select  * from tabrotinacontato where cliente_idcliente = " + idCliente + ";");
-    }
-
-    public static int GetIndice() {
+    
+     public static int GetIndice() {                     
         return indice;
     }
-
-    public void TabelaRotina(String Sql) {
-
-        try {
-
+     
+     public void TabelaRotina(String Sql){
+        
+        try { 
+            
             stmt = getConnection().createStatement();
-            ArrayList dados = new ArrayList();
-            String[] Colunas = {"Código", "Data", "Hora", "Descrição"};
-
+            ArrayList dados = new ArrayList();               
+            String [] Colunas = {"Código","Código do cliente", "Código do usuario", "Data", "Hora", "Descrição"};
+               
             ResultSet rs;
-            rs = stmt.executeQuery(Sql);
+            rs = stmt.executeQuery(Sql);            
+            rs.first();
+            
+                    do{
+                       dados.add(new Object[]{rs.getObject("idRotinaContato"),rs.getObject("cliente_idcliente"),
+                                              rs.getObject("tabusuario_id_usuario"),rs.getObject("dataRotina"), 
+                                              rs.getObject("horaRotina"),rs.getObject("descricaoRotina")});            
+                    }while(rs.next());
 
-            while (rs.next()){
-                
-                dados.add(new Object[]{
-                    rs.getObject("idRotinaContato"),
-                    rs.getObject("dataRotina"),
-                    rs.getObject("horaRotina"), 
-                    rs.getObject("descricaoRotina")});
-            }
-
-            for (int i = 0; i < 4; i++) {
-                ModeloTabela modelo = new ModeloTabela(dados, Colunas);
-                jTableListarRotinas.setModel(modelo);
-                jTableListarRotinas.getColumnModel().getColumn(i).setPreferredWidth(150);
-                jTableListarRotinas.getColumnModel().getColumn(i).setResizable(false);
-                jTableListarRotinas.getTableHeader().setReorderingAllowed(false);
-                jTableListarRotinas.setAutoResizeMode(jTableListarRotinas.AUTO_RESIZE_OFF);
-                jTableListarRotinas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-            }
-
+                    for (int i = 0; i < dados.size(); i++){
+                        ModeloTabela modelo = new ModeloTabela(dados, Colunas);
+                        jTableListarRotinas.setModel(modelo);
+                        jTableListarRotinas.getColumnModel().getColumn(i).setPreferredWidth(150);
+                        jTableListarRotinas.getColumnModel().getColumn(i).setResizable(false);
+                        jTableListarRotinas.getTableHeader().setReorderingAllowed(false);
+                        jTableListarRotinas.setAutoResizeMode(jTableListarRotinas.AUTO_RESIZE_OFF);
+                        jTableListarRotinas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                    }
+                    
         } catch (SQLException ex) {
             Logger.getLogger(ExibeCliente.class.getName()).log(Level.SEVERE, null, ex);
-
-        } catch (Exception erro) {
+            
+        } catch (Exception erro){
             Logger.getLogger(ExibeCliente.class.getName()).log(Level.SEVERE, null, erro);
-        }
+        }          
     }
 
-    private void verificaPagina(){
-        if ((this.telaDetalharCliente != null)) {
-            this.telaDetalharCliente.setEnabled(true);
-            this.telaDetalharCliente.toFront();
-        }
-    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -99,50 +87,88 @@ public class ExibeRotina extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableListarRotinas = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowClosed(java.awt.event.WindowEvent evt) {
-                formWindowClosed(evt);
-            }
-        });
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jTableListarRotinas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {},
-                {},
-                {},
-                {}
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
             },
             new String [] {
-
+                "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
         jScrollPane1.setViewportView(jTableListarRotinas);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 543, 190));
-
-        jLabel1.setFont(new java.awt.Font("Raavi", 1, 18)); // NOI18N
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/rotinacont.png"))); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel1.setText("Rotinas Cadastradas");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 50, -1, -1));
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/leiaute/img1.png"))); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -10, 670, 110));
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 568, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(173, 173, 173))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(20, 20, 20)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(56, Short.MAX_VALUE))
+        );
 
         pack();
-        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
-        verificaPagina();
-    }//GEN-LAST:event_formWindowClosed
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(ExibeRotina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(ExibeRotina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(ExibeRotina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(ExibeRotina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new ExibeRotina().setVisible(true);
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTableListarRotinas;
     // End of variables declaration//GEN-END:variables
