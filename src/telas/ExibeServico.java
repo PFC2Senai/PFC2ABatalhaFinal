44506ac39c70bb20fcf9ee54601f90a0442b9efe
graciele <graciele@graciele-PC>
2016-02-10 12:15:04 -0200

    Merge origin/master
    Conflicts:
    	src/funcoes/ConexaoPermissoes.java

--- src/telas/ExibeServico.java
+++ src/telas/ExibeServico.java
@@ -73,7 +73,7 @@ public class ExibeServico extends javax.swing.JFrame {
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(204, 255, 204));
+        jPanel1.setBackground(new java.awt.Color(223, 237, 253));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jTableListarServicos.setModel(new javax.swing.table.DefaultTableModel(
